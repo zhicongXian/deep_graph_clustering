9ab24373e00abf35e3914c32588c2bc4ebefb05d
zhicong.xian <zhicong.xian@hotmail.de>
2025-03-11 14:35:13 +0100

    commit results

--- playgrounds/ssc_heapmap_visualize.py
+++ playgrounds/ssc_heapmap_visualize.py
@@ -1,11 +1,13 @@
 # Created by zhicong.xian at 14:49 07.03.2025 using PyCharm
 import scipy as sp
+import numpy as np
 
 x_filepath = "../datasets/affinity_matrix_from_senet_sparse_20000.npz"
+y_filepath = "../datasets/senet_label_20000.csv"
 x = sp.sparse.load_npz(x_filepath).toarray()
-
+y = np.loadtxt(y_filepath)
 import plotly.express as px
 import plotly
 
-fig = px.imshow(x[:1000])
+fig = px.imshow(x[y == 0][:1000, :1000])
 plotly.offline.plot(fig, filename="affinity_matrix_from_senet_sparse_20000_ssc_heatmap" + '.html', auto_open=True)
