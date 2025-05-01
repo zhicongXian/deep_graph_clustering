bf1a8cb609dc39783efc13138c59c807b63d6ba1
zhicong.xian <zhicong.xian@hotmail.de>
2025-05-01 18:22:27 +0200

    save results

--- fau_alex_job_script.sh
+++ fau_alex_job_script.sh
@@ -11,5 +11,5 @@ module load python/3.9-anaconda
 module load gcc/11.2.0
 # source $WORK/python_virtual_envs/doubly_robust_env/bin/activate # activate the python virtual environment
 conda activate clustering_env
-python3 ./main.py --dataset=SeNet --data_path=./datasets/affinity_matrix_from_senet_sparse_1000.npz --label_path=./datasets/senet_label_1000.csv >> output_main_lsenet.txt
+python3 ./main.py --dataset=SeNet --data_path=./datasets/affinity_matrix_from_senet_sparse_1000.npz --label_path=./datasets/senet_label_1000.csv >> output_main_lsenet_2.txt
 # ./cuda_application
