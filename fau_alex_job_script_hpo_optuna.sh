25e399237d2bf1c35cf2e66b0ee2515d805e2149
zhicong.xian <zhicong.xian@hotmail.de>
2025-05-16 15:37:09 +0200

    commit the following sentences

--- fau_alex_job_script_hpo_optuna.sh
+++ fau_alex_job_script_hpo_optuna.sh
@@ -2,8 +2,8 @@
 #
 # SBATCH --gres=gpu:a100:1
 #SBATCH --time=24:00:00
-#SBATCH --job-name=lsenet_hpo
-#SBATCH --output=res_hpo.txt
+#SBATCH --job-name=lsenet_optuna
+#SBATCH --output=res_optuna.txt
 
 unset SLURM_EXPORT_ENV
 
@@ -11,5 +11,5 @@ module load python/3.9-anaconda
 module load gcc/11.2.0
 # source $WORK/python_virtual_envs/doubly_robust_env/bin/activate # activate the python virtual environment
 conda activate clustering_env
-python3 ./main_with_optuna.py --dataset=SeNet --data_path=./datasets/affinity_matrix_from_senet_sparse_1000.npz --label_path=./datasets/senet_label_1000.csv>> output_main_lsenet_hpo.txt
+python3 ./main_with_optuna.py --dataset=SeNet --data_path=./datasets/affinity_matrix_from_senet_sparse_1000.npz --label_path=./datasets/senet_label_1000.csv>> output_main_lsenet_hpo_optuna.txt
 # ./cuda_application
